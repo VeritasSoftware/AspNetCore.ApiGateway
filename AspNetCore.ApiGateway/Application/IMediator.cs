a53e38faf8d8c1efcd51cb57a244824860b91f91
Shan Negi <shan.negi@zware.com.au>
2019-11-26 10:22:41 +1100

    cleanup

--- AspNetCore.ApiGateway/Application/IMediator.cs
+++ AspNetCore.ApiGateway/Application/IMediator.cs
@@ -6,13 +6,6 @@ namespace AspNetCore.ApiGateway
 {
     public interface IMediator
     {
-        //IMediator AddRoute(string key,
-        //                string path, 
-        //                string verb, 
-        //                Type responseType, 
-        //                Type requestType = null, 
-        //                Func<RouteInfo, Task<object>> exec = null);
-
         IMediator AddRoute(string key, RouteInfo routeInfo);
 
         IMediator AddRoute(string key, Func<ApiInfo, RouteInfo, HttpRequest, Task<object>> exec);
